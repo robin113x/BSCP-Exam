c202d7102a24ae9728345b490b5f43ab1e79eb58
robin113x <robin113x@gmail.com>
2025-07-10 17:21:23 -0400

    Robin h00d

---
+++ AutoPush.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+while true; do
+
+    if [[ -n $(git status --porcelain) ]]; then
+        echo "Changes detected in repository. Committing and pushing changes..."
+        git pull
+        git add -A
+        git commit -m "Robin h00d"
+        git push
+        echo "Changes committed and pushed successfully."
+    fi
+    sleep 5
+done
