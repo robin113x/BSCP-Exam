eec794639a373d862d4427a479b8e5a4a1b31136
SatyajitShinde0333 <shindesatyajeet2000@gmail.com>
2025-08-21 02:02:11 +0530

    Robin h00d

---
+++ AutoPushWind.ps1
@@ -0,0 +1,12 @@
+while ($true) {
+    $status = git status --porcelain
+    if ($status) {
+        Write-Output "Changes detected in repository. Committing and pushing changes..."
+        git pull origin main --rebase
+        git add -A
+        git commit -m "Robin h00d"
+        git push origin main
+        Write-Output "Changes committed and pushed successfully."
+    }
+    Start-Sleep -Seconds 5
+}
